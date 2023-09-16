b7027d1afca9866c71c2991b5ae1f49e08fa1683
Mohammad Ghorbani <moohammad.ghoorbani@gmail.com>
2023-09-16 13:08:27 +0330

    feat: change dates to std format

---
+++ std-names.py
@@ -0,0 +1,21 @@
+import os
+
+# Get the current working directory (where the script is executing)
+directory_path = os.getcwd()
+
+# Get a list of all files in the directory
+file_list = os.listdir(directory_path)
+
+# Filter the list to include only files with the specified extension (e.g., .mp4)
+file_list = [file for file in file_list if file.endswith('.mp4')]
+
+# Rename the files with the reversed date order
+for filename in file_list:
+    parts = filename.split('.')
+    if len(parts) == 4:
+        day, month, year, extension = parts
+        if int(day) > 31:
+            print("skipped")
+            continue
+        new_filename = f"{year}.{month}.{day}.{extension}"
+        os.rename(os.path.join(directory_path, filename), os.path.join(directory_path, new_filename))
